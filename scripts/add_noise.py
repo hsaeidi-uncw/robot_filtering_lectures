def881ef4525c5d438b406dc88edde63b9a049d2
hsaeidi-uncw <saeidih@uncw.edu>
2022-03-24 13:45:46 +0000

    noise generator

---
+++ scripts/add_noise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rospy
+# we are going to read turtlesim/Pose messages this time
+from turtlesim.msg import Pose
+# for generating random noise
+import random
+
+
+pos_msg = Pose()
+
+def pose_callback(data):
+	global pos_msg
+	# update the current message
+	pos_msg = data
+	# add some noise to the x coordinate
+	noise_amplitude = 0.25 # in meters
+	pos_msg.x += random.uniform(-noise_amplitude, noise_amplitude)
+	
+	
+if __name__ == '__main__':
+	# initialize the node
+	rospy.init_node('add_noise', anonymous = True)
+	# add a subscriber to it to read the position information
+	pos_sub = rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+	# add a publisher with a new topic using the Shortpos message
+	pos_pub = rospy.Publisher('/turtle1/noisy_pose', Pose, queue_size = 10)
+	# set a 10Hz frequency for this loop
+	loop_rate = rospy.Rate(10)
+
+	while not rospy.is_shutdown():
+		# publish the message
+		pos_pub.publish(pos_msg)
+		# wait for 0.1 seconds until the next loop and repeat
+		loop_rate.sleep()
