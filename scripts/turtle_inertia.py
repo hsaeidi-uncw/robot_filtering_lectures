1b96a4a6ada2342167c64e47cf6c6a773cc91b5f
hsaeidi-uncw <saeidih@uncw.edu>
2022-03-24 19:12:58 +0000

    keyboard fix

--- scripts/turtle_inertia.py
+++ scripts/turtle_inertia.py
@@ -5,15 +5,21 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
+# for reading the force commands
 force = 0.0
 
 pos_cur = float()
 
+key_released = True # by default we assume that they arrow key on keyboard is not pressed
+ 
 # get the force value
 def get_force(data):
 	global force
-	force = data.linear.x 
+	global key_released
+	force = data.linear.x # update the force value 
+	key_released = False # now the key is pressed
 	
+# read the position value
 def pose_callback(data):
 	global pos_cur
 	pos_cur = data.x
@@ -38,19 +44,22 @@ if __name__ == '__main__':
 	vel_cmd = Twist()
 	# define the paremeters of the equations 
 	mass = 1.0 # m in the equations (kg)
-	damping = 3 # b in the equations (Ns/m)
+	damping = 1.0 # b in the equations (Ns/m)
 	vel_next = 0.0 # initialize the next velocity
 	pos_prev = 0.0 # initialize the previous position
 	# run this control loop regularly
 	while not rospy.is_shutdown():
+		print(key_released)
+		if key_released:
+			force = 0.0 # make sure to remove non-zero commands when key is released
 		vel_cur = (pos_cur - pos_prev)/dt
 		vel_next = dt/mass*(force - damping*vel_cur) + vel_cur
 		print('force = ', force, ' (N) and linear velocity=', vel_cur, 'm/s', 'and next velocity=', vel_next, 'm/s')
 		# set the linear (forward/backward) velocity command
 		vel_cmd.linear.x = vel_next
-		# set the angular (heading) velocity command
 		cmd_pub.publish(vel_cmd)
 		# update the pose for next iteration
 		pos_prev = pos_cur
+		key_released = True # assume the key is not pressed unless we receive a new command which proves otherwise
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
